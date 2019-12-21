f901653c1c811efed2baf59d2b37e88404c2a4a3
Fabyo Silveira Amorim <silveira.fabyo@outlook.com>
2019-12-20 21:43:23 -0300

    Inclusão de projeto de teste.

---
+++ ConsoleApplicationDti/ConsoleApplicationDtiTests/UnitMethodsTest.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/Client.h"
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace ConsoleApplicationDtiTests
+{		
+	TEST_CLASS(UnitMethodsTest)
+	{
+	public:
+		
+		TEST_METHOD(ClientConstructor)
+		{
+			string name = "Fabyo";
+			string address = "Conceicao Mato Dentro";
+			string phone = "997278141";
+			string email = "silveira.fabyo@outlook.com";
+			string birthDate = "11/02/1999";
+
+			Client clientObject("Fabyo", "Conceicao Mato Dentro", "997278141", "silveira.fabyo@outlook.com", "11/02/1999");
+			
+			Assert::AreEqual(name, clientObject.name);
+			Assert::AreEqual(address, clientObject.address);
+			Assert::AreEqual(phone, clientObject.phone);
+			Assert::AreEqual(email, clientObject.email);
+			Assert::AreEqual(birthDate, clientObject.birthDate);
+		}
+
+	};
+}
