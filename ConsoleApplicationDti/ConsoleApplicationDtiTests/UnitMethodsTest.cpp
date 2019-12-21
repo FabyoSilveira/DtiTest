dbb2311ed8da33bcf575c0af34d0c5fcad2e2074
Fabyo Silveira Amorim <silveira.fabyo@outlook.com>
2019-12-20 22:27:55 -0300

    remocao projeto teste

--- ConsoleApplicationDti/ConsoleApplicationDtiTests/UnitMethodsTest.cpp
+++ ConsoleApplicationDti/ConsoleApplicationDtiTests/UnitMethodsTest.cpp
@@ -20,13 +20,13 @@ namespace ConsoleApplicationDtiTests
 			string email = "silveira.fabyo@outlook.com";
 			string birthDate = "11/02/1999";
 
-			Client clientObject("Fabyo", "Conceicao Mato Dentro", "997278141", "silveira.fabyo@outlook.com", "11/02/1999");
+			Client* clientObject = new Client("Fabyo", "Conceicao Mato Dentro", "997278141", "silveira.fabyo@outlook.com", "11/02/1999");
 			
-			Assert::AreEqual(name, clientObject.name);
-			Assert::AreEqual(address, clientObject.address);
-			Assert::AreEqual(phone, clientObject.phone);
-			Assert::AreEqual(email, clientObject.email);
-			Assert::AreEqual(birthDate, clientObject.birthDate);
+			Assert::AreEqual(name, clientObject->name);
+			Assert::AreEqual(address, clientObject->address);
+			Assert::AreEqual(phone, clientObject->phone);
+			Assert::AreEqual(email, clientObject->email);
+			Assert::AreEqual(birthDate, clientObject->birthDate);
 		}
 
 	};
